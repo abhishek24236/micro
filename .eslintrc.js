d4316a2dc7b5055834aaaa5415577b65af17a228
abhishek24236 <38054721+abhishek24236@users.noreply.github.com>
2022-06-02 14:33:46 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,3 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  extends: [
-    "airbnb-base",
-    "plugin:vue/recommended"
-  ],
-  plugins: [
-    "vue",
-    "html"
-  ],
-  rules: {
-    "space-before-function-paren": [
-      2,
-      {
-        "anonymous": "always",
-        "named": "never"
-      }
-    ],
-    "vue/html-closing-bracket-spacing": "off",
-    "vue/singleline-html-element-content-newline": "off",
-  },
-};
+{
+    "extends": "react-app"
+}
